c52a624ffe76b2002eb2484bc27e6fbf04ae6bb2
SongMinWoo <20171185@vision.hoseo.edu>
2022-05-07 21:25:13 +0900

    new login

---
+++ app/src/main/java/com/example/myapplication/User_MainScreen.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class User_MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_mainscreen); // 메인화면 시작
+
+        Button start_btn = (Button) findViewById(R.id.main_start); // 시작하기 버튼
+
+        // 시작하기 -> 로그인화면 전환
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), User_Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
