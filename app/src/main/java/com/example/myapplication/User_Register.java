3f7a59cfcae2e1158b794dfad435303449ece731
SongMinWoo <20171185@vision.hoseo.edu>
2022-05-26 17:45:51 +0900

    change

--- app/src/main/java/com/example/myapplication/User_Register.java
+++ app/src/main/java/com/example/myapplication/User_Register.java
@@ -50,30 +50,24 @@ public class User_Register extends AppCompatActivity {
         setContentView(R.layout.user_register);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(User_Register.this);
-        EditText emailText = (EditText) findViewById(R.id.emailText); // 이메일 입력 텍스트
-        Button duplicateID = (Button) findViewById(R.id.email_confirm_btn); // 이메일 중복 체크 버튼
+        EditText idText = (EditText) findViewById(R.id.idText); // 아이디 입력 텍스트
+        Button duplicateID = (Button) findViewById(R.id.id_confirm_btn); // 아이디 중복 체크 버튼
         EditText pwd = (EditText) findViewById(R.id.passwordText); // 비밀번호 입력 텍스트
         EditText pwd_cfm = (EditText) findViewById(R.id.password_confirm); // 비밀번호 확인 텍스트
         Button pwd_check = (Button) findViewById(R.id.pwd_confirm_btn); // 비밀번호 확인 버튼
-        EditText name = (EditText) findViewById(R.id.nameText); // 이름 입력 텍스트
+        EditText email = (EditText) findViewById(R.id.emailText); // 이메일 텍스트
         Button register_btn = (Button) findViewById(R.id.register_btn); // 회원가입 버튼
 
 
-        // 이메일 증복체크 버튼(데이터 안에 있는 아이디 중복 검사) -> 데이터베이스 추가시 구현 예정!!
+        // 아이디 증복체크 버튼(데이터 안에 있는 아이디 중복 검사) -> 데이터베이스 추가시 구현 예정!!
         duplicateID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 이메일을 입력하지 않고 중복체크 버튼을 누를시 이메일을 입력하라는 창이뜸 (민우)
-                if (emailText.getText().toString().length() == 0 ) {
-                    Toast.makeText(User_Register.this, "이메일를 입력하세요", Toast.LENGTH_SHORT).show();
-                    emailText.requestFocus();
-                    return;
-                }
                 builder.setTitle("확인 메시지");
-                builder.setMessage("사용 가능한 이메일입니다.");
+                builder.setMessage("사용 가능한 아이디입니다.");
                 builder.setPositiveButton("확인",null);
                 builder.create().show();
-                id_flag = 1; // 이메일 중복 검사 플래그 1로 변경
+                id_flag = 1; // 아이디 중복 검사 플래그 1로 변경
             }
         });
 
@@ -83,19 +77,13 @@ public class User_Register extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (pwd.getText().toString().equals(pwd_cfm.getText().toString())) {
-                    // 비밀번호와 비밀번호 확인란에 아무것도 안적고 확인 버튼 누를시 입력하라는 창이뜸 (민우)
-                    if(pwd.getText().toString().length() == 0){
-                        Toast.makeText(User_Register.this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show();
-                        pwd.requestFocus();
-                        return;
-                    }
                     builder.setTitle("확인 메시지");
                     builder.setMessage("비밀번호 일치 ^^");
                     builder.setPositiveButton("확인",null);
                     builder.setNegativeButton("취소",  null);
                     builder.create().show();
                     pwd_flag = 1; // 비밀번호 확인 검사 플래그 1로 변경
-                } else{
+                } else {
                     builder.setTitle("확인 메시지");
                     builder.setMessage("비밀번호 불일치!");
                     builder.setPositiveButton("확인",null);
@@ -109,10 +97,10 @@ public class User_Register extends AppCompatActivity {
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // 이메일 입력 필수
-                if (emailText.getText().toString().length() == 0 ) {
-                    Toast.makeText(User_Register.this, "이메일를 입력하세요", Toast.LENGTH_SHORT).show();
-                    emailText.requestFocus();
+                // 아이디 입력 필수
+                if (idText.getText().toString().length() == 0 ) {
+                    Toast.makeText(User_Register.this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+                    idText.requestFocus();
                     return;
                 }
                 // 비밀번호 입력 필수
@@ -127,16 +115,16 @@ public class User_Register extends AppCompatActivity {
                    pwd_cfm.requestFocus();
                     return;
                 }
-                // 닉네임 입력 필수
-                if (name.getText().toString().length() == 0 ) {
-                    Toast.makeText(User_Register.this, "닉네임을 입력하세요", Toast.LENGTH_SHORT).show();
-                    name.requestFocus();
+                // 이메일 입력 필수
+                if (email.getText().toString().length() == 0 ) {
+                    Toast.makeText(User_Register.this, "이메일을 입력하세요", Toast.LENGTH_SHORT).show();
+                    email.requestFocus();
                     return;
                 }
-                // 이메일 중복 체크 필수
+                // 아이디 중복 체크 필수
                 if(id_flag == 0)
                 {
-                    Toast.makeText(User_Register.this, "이메일 중복 검사를 해주세요", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(User_Register.this, "아이디 중복 검사를 해주세요", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 // 비밀번호 일치 검사 필수
