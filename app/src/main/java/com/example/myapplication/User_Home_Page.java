245bcea8ffb79c894375c4f52a2e0d0a74e485de
SongMinWoo <20171185@vision.hoseo.edu>
2022-05-27 00:06:00 +0900

    fragment intent 시도

--- app/src/main/java/com/example/myapplication/User_Home_Page.java
+++ app/src/main/java/com/example/myapplication/User_Home_Page.java
@@ -13,6 +13,9 @@ import androidx.fragment.app.Fragment;
 
 public class User_Home_Page extends Fragment {
 
+    private View view;
+
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
@@ -21,6 +24,60 @@ public class User_Home_Page extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+    protected void onCreateView(@Nullable Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+        Button today_keyword = (Button) view.findViewById(R.id.today_keword); // 실시간 맛집 키워드 버튼
+        Button today_restourant = (Button) view.findViewById(R.id.today_restourant); // 오늘의 맛집 버튼
+
+        // 메인화면 -> 실시간 맛집 키워드 화면 전환
+        today_keyword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Restourant_Keyword.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        // 메인화면 -> 오늘의 맛집 화면 전환
+        today_restourant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Today_Restourant.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setContentView(int home_page) {
+        Button today_keyword = (Button) view.findViewById(R.id.today_keword); // 실시간 맛집 키워드 버튼
+        Button today_restourant = (Button) view.findViewById(R.id.today_restourant); // 오늘의 맛집 버튼
+
+        // 메인화면 -> 실시간 맛집 키워드 화면 전환
+        today_keyword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Restourant_Keyword.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        // 메인화면 -> 오늘의 맛집 화면 전환
+        today_restourant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Today_Restourant.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/myapplication/User_Myfavorite_Page.java
+++ app/src/main/java/com/example/myapplication/User_Myfavorite_Page.java
@@ -12,7 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-public class User_Myfavorite_Page extends Fragment {
+public class User_Myfavorite_Page extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
