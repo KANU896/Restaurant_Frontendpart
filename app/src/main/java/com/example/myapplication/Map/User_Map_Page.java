afb23a3e2315d844efb5892a67a9206442aa0764
SongMinWoo <20171185@vision.hoseo.edu>
2022-08-23 00:44:26 +0900

    카카오 맵 구현중
    2

--- app/src/main/java/com/example/myapplication/Map/User_Map_Page.java
+++ app/src/main/java/com/example/myapplication/Map/User_Map_Page.java
@@ -4,36 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.libraries.maps.MapView;
+import net.daum.mf.map.api.MapView;
+
 
 public class User_Map_Page extends Fragment /*implements OnMapReadyCallback*/ {
-    private View view;
-    private GoogleMap mMap;
 
     @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public void onCreate(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_page);
 
-        net.daum.mf.map.api.MapView mapView = new MapView(this);
+        MapView mapView = new MapView(getContext());
 
-        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+        View root = inflater(R.layout.map_page, container, false);
+        //RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.MapView);
+        ViewGroup mapViewContainer = (ViewGroup)root.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
     }
+
+    private View inflater(int map_page, ViewGroup container, boolean b) {
+        return null;
+    }
+
+    private void setContentView(int map_page) {
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
