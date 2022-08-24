519250146081cfebd2be86bdb23d80d2aecc4993
SongMinWoo <20171185@vision.hoseo.edu>
2022-08-25 00:58:03 +0900

    카카오 맵 수정
    fragment에 맞게 java 수정
    지도 들어갈시 팅김 현상 생김

--- app/src/main/java/com/example/myapplication/Main_Frame.java
+++ app/src/main/java/com/example/myapplication/Main_Frame.java
@@ -179,4 +179,5 @@ public class Main_Frame extends AppCompatActivity {
         }
 
     }
+
 }
--- app/src/main/java/com/example/myapplication/Map/User_Map_Page.java
+++ app/src/main/java/com/example/myapplication/Map/User_Map_Page.java
@@ -10,22 +10,52 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 
 public class User_Map_Page extends Fragment /*implements OnMapReadyCallback*/ {
 
-    @Nullable
-    public void onCreate(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_page);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        MapView mapView = new MapView(getContext());
-
+        //Inflage the layout for this fragment
         View root = inflater(R.layout.map_page, container, false);
-        //RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+
+        //지도
+        MapView mapView = new MapView(getActivity());
         ViewGroup mapViewContainer = (ViewGroup) root.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.map_page);
+
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(4, true);
+
+        //마커 찍기
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
+
+
+
+
+        //RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+
+        return root;
     }
 
     private View inflater(int map_page, ViewGroup container, boolean b) {
@@ -40,5 +70,7 @@ public class User_Map_Page extends Fragment /*implements OnMapReadyCallback*/ {
         super.onViewCreated(view, savedInstanceState);
 
     }
+
+
 
 }
