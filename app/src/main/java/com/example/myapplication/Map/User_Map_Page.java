bc0974713107a9cc8cff835d9d0298d74c79b112
SongMinWoo <20171185@vision.hoseo.edu>
2022-08-30 23:46:21 +0900

    .
    .

--- app/src/main/java/com/example/myapplication/Map/User_Map_Page.java
+++ app/src/main/java/com/example/myapplication/Map/User_Map_Page.java
@@ -22,8 +22,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.myapplication.R;
-
-import net.daum.android.map.MapViewEventListener;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
@@ -45,7 +43,6 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
 
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -55,65 +52,47 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
             requestPermissions(new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, 101);
         }
 
-
-
         //Inflage the layout for this fragment
         View root = inflater.inflate(R.layout.map_page, container, false);
 
         //지도
-        MapView mapView = new MapView(getActivity());
-        ViewGroup mapViewContainer = (ViewGroup) root.findViewById(R.id.map_view);
+        mapView = new MapView(getActivity());
+        mapViewContainer = (ViewGroup) root.findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
         mapView.setMapViewEventListener(this);
 
 
-        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting();
         }else {
             checkRunTimePermission();
         }
 
-
-        /*
-        // 중심점 변경
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
-
-        // 줌 레벨 변경
-        mapView.setZoomLevel(4, true);
-
-        // 마커 찍기
-        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);
-        MapPOIItem marker = new MapPOIItem();
-        marker.setItemName("Default Marker");
-        marker.setTag(0);
-        marker.setMapPoint(MARKER_POINT);
-        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
-        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-
-        mapView.addPOIItem(marker);
-*/
-
         return root;
-
-
-
     }
 
-    @Override
+    /*@Override
     public void onDestroy() {
         super.onDestroy();
         mapViewContainer.removeAllViews();
-        /*MapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
-        MapView.setShowCurrentLocationMarker(false);*/
 
-    }
+    }*/
+
+
 
 
 
     public void onCurrentLocationUpdate(MapView mapView, MapPoint currentLocation, float accuracyInMeters){
         MapPoint.GeoCoordinate mapPointGeo = currentLocation.getMapPointGeoCoord();
         Log.i(LOG_TAG, String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, accuracyInMeters));
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("현 위치");
+        marker.setMapPoint(currentLocation);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
     }
 
 
@@ -139,7 +118,6 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
     public void onRequestPermissionsResult(int permsRequestCode,
                                            @NonNull String[] permissions,
                                            @NonNull int[] grandResults){
-        this.context = context;
 
         if(permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
 
@@ -162,9 +140,10 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
             else{
                 if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), REQUIRED_PERMISSIONS[0])){
                     Toast.makeText(User_Map_Page.context, "퍼미션이 거부되었습니다.", Toast.LENGTH_LONG).show();
-                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    /*FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                     fragmentManager.beginTransaction().remove(User_Map_Page.this).commit();
-                    fragmentManager.popBackStack();
+                    fragmentManager.popBackStack();*/
+                    getActivity().finish();
                 }
                 else{
                     Toast.makeText(User_Map_Page.context, "퍼미션이 거부되었습니다.", Toast.LENGTH_LONG).show();
@@ -177,9 +156,7 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
     public void checkRunTimePermission(){
         //런타임 퍼미션 처리
         // 1. 위치 퍼미션을 가지고 있는지 체크
-        int hasFineLocationPermission = ContextCompat.checkSelfPermission(getActivity()
-                ,
-                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION);
 
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED ) {
             // 2. 이미 퍼미션을 가지고 있다면
@@ -192,13 +169,11 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
                 // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있음.
                 Toast.makeText(User_Map_Page.context, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
                 // 3-3. 사용자에게 퍼미션 요청. 요청 결과는 onRequestPermissionResult에서 수신.
-                ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE);
+                ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
             } else {
                 // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 수행.
                 // 요청 결과는 onRequestPermissionResult에서 수신.
-                ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE);
+                ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
             }
         }
     }
@@ -206,7 +181,7 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
     // GPS 활성화를 위한 메소드
     private void showDialogForLocationServiceSetting() {
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(User_Map_Page.context);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("위치 서비스 비활성화");
         builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
                 + "위치 설정을 수정하시겠습니까?");
@@ -252,7 +227,10 @@ public class User_Map_Page extends Fragment implements  MapView.CurrentLocationE
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
     }
+
+
 
     @Override
     public void onMapViewInitialized(MapView mapView) {
